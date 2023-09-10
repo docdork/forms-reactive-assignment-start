8b63fff74e9c88ce373d77142541fa0061a63c79
David_Holliday <dhholliday@icloud.com>
2023-09-10 17:14:37 +0100

    Finish.  Still problems with validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { CustomValidators } from "./custom-validators";
+// import { CustomValidators } from "./custom-validators";
 
 @Component({
   selector: "app-root",
@@ -12,11 +12,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(
-        null,
-        [Validators.required, CustomValidators.invalidProjectName],
-        CustomValidators.asyncInvalidProjectName
-      ),
+      projectName: new FormControl(null, [Validators.required]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       projectStatus: new FormControl("critical"),
     });
